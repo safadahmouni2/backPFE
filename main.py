284c626a23860e24f95d477f9354329bed7b3105
polytech2019 <dahmounisafa3@gmail>
2021-08-21 09:18:48 +0100

    rest api with functional ML :algo SVM NB LR

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 
 # Import the Flask module that has been installed.
+import joblib
 from flask import Flask, jsonify
 from data import data_distribution,influencedist,persondist,accuracy,topicmodeling,comparaison
 import sys
 from flask_cors import CORS
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn import svm
+from flask import request
 
 # Creating a new "app" by using the Flask constructor. Passes __name__ as a parameter.
 app = Flask(__name__)
@@ -38,7 +42,56 @@ def ftopicmodeling():
 
 @app.route("/application")
 def appl():
-    return jsonify({"result":"result of prediction"})
+    text = request.args.get("textinput")
+    algorithm = request.args.get("algorithm")
+    model = get_model(algorithm)
+    result = predict(text,model)
+    return jsonify({"result":result})
+
+
+# load the model from disk
+# load the model from disk
+
+def get_politician(index):
+    politicians = [
+        "",
+        "قيس سعيد",
+        "عبير موسي",
+        "هشام المشيشي",
+        "الصافي سعيد",
+        "راشد الغنوشي",
+        "محمد عبو",
+        "سيف الدين مخلوف",
+        "يوسف الشاهد",
+        "ياسين العياري",
+        "فيصل التبيني"
+    ]
+    return politicians[index]
+
+def get_model(algorithm):
+    if algorithm=="nb":
+        loaded_model= joblib.load("modelnb.sav")
+    elif algorithm=="svm":
+         loaded_model= joblib.load("modelsvm.sav")
+    elif algorithm=="lr":
+         loaded_model= joblib.load("modelLogisticRegression.sav")
+    elif algorithm=="rf":
+         loaded_model= joblib.load("modelRandomForest.sav")
+    else:
+        loaded_model= joblib.load("modelnb.sav")
+    return loaded_model
+
+def predict(text,model):
+    #loaded_model= joblib.load("modelnb.sav")
+    tfidfvect = joblib.load("tfidfvect.sav")  
+    #text = "الارادة السياسية كانت السبب في إنتهاء الارهاب في تونس بفضل تعاون راشد الغنوشي و يوسف الشاهد"
+
+    a = []
+    a.append(text)
+    ap = tfidfvect.transform(a)
+    result =model.predict(ap)
+    politician_name = get_politician(int(result[0]))
+    return {"politician_name" :str(politician_name), "influence":str(result[0])}
 
 if __name__ == "__main__":
     app.config['JSON_AS_ASCII'] = False
